6a28760bf8b0546a4c35180a199afb35b439f3de
MikeStork <44142875+MikeStork@users.noreply.github.com>
2021-11-01 15:10:47 +0100

    asd

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Projects.css';
 import Sine from './Sine';
 import Content from './Content';
+import Window from './Window';
 const Projects = () => {
     return (
         <div>
             <Sine></Sine>
             {/* <Content text="jajetz" offset="25vh"></Content> */}
+            <Window/>
         </div>
     )
 }
---
+++ src/components/Window.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Window.css";
+const Window = () => {
+  return (
+    <div className="contain">
+      <div className="windowoutline">
+        <div className="window">
+            asdasdasd
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Window;
